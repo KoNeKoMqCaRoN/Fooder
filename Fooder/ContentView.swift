9f1ccff9c1bf4ff8b9781354c6c7d1ab3de0e978
KoNeKo_MqCaRon <shoukun733@gmail.com>
2025-06-23 11:04:25 +0900

    Initial Commit

---
+++ Fooder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fooder
+//
+//  Created by cmStudent on 2025/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fooder/FooderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooderApp.swift
+//  Fooder
+//
+//  Created by cmStudent on 2025/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FooderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
