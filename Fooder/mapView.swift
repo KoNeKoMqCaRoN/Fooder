42650d3aee0351d31dd4f65011935f0eba4384b6
KoNeKo_MqCaRon <shoukun733@gmail.com>
2025-06-27 18:16:19 +0900

    mapViewを作った

---
+++ Fooder/mapView.swift
@@ -0,0 +1,18 @@
+//
+//  mapView.swift
+//  Fooder
+//
+//  Created by cmStudent on 2025/06/27.
+//
+
+import SwiftUI
+
+struct mapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mapView()
+}
