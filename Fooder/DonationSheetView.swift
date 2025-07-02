f07bd72c4fd10b4c0ed067df8d6f1dfbeb1df678
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-07-02 12:27:22 +0900

    [ファイル作成]　DonationSheetView

---
+++ Fooder/DonationSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  DonationSheetView.swift
+//  Fooder
+//
+//  Created by cmStudent on 2025/07/02.
+//
+
+import SwiftUI
+
+struct DonationSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DonationSheetView()
+}
