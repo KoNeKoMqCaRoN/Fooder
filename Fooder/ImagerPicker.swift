a62040a3448145d0063aa760f5f5244ab5215baf
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-07-02 12:45:56 +0900

    [実装] ImagePicker
    カメラとPhotoLibraryから写真選択するため

--- Fooder/ImagerPicker.swift
+++ Fooder/ImagerPicker.swift
@@ -5,4 +5,49 @@
 //  Created by cmStudent on 2025/07/02.
 //
 
-import Foundation
+import SwiftUI
+import UIKit
+
+struct ImagerPicker: UIViewControllerRepresentable {
+    
+    @Environment(\.presentationMode) var presentationMode
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    var onImageSelected: (UIImage) -> Void
+    
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagerPicker
+        
+        init(_ parent: ImagerPicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.onImageSelected(image)
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+}
