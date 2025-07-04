59936cfe59011e9cb90b345330cfd6c3da5d41ba
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-07-04 15:31:40 +0900

    RequestSheetViewの作成

--- Fooder/RequestSheetView.swift
+++ Fooder/RequestSheetView.swift
@@ -6,13 +6,98 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct RequestSheetView: View {
+    @Environment(\.presentationMode) private var presentationMode
+    @State private var showConfirmationView = false
+    @StateObject private var vm: RequestSheetViewModel = RequestSheetViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                CustomSectionView("食材名", required: true, showWrongInputError: vm.formValidateResult == .invalidField(.foodName)) {
+                    CustomTextFieldView("例：りんご", text: $vm.foodName)
+                }
+                
+                HStack {
+                    CustomSectionView("数量", required: true, showWrongInputError: vm.formValidateResult == .invalidField(.amount)) {
+                        CustomTextFieldView("例：4", text: $vm.amount, keyboardType: .numberPad)
+                    }
+                    
+                    CustomSectionView("単位", required: true, showWrongInputError: vm.formValidateResult == .invalidField(.unit)) {
+                        CustomTextFieldView("例：個", text: $vm.unit)
+                    }
+                }
+                
+                CustomSectionView("優先度", required: true, showWrongInputError: vm.formValidateResult == .invalidField(.priority)) {
+                    PriorityPickerView(selectedPriority: $vm.selectedPriority, showUrgentPriority: $vm.showUrgentPriority)
+                } comment: {
+                    Text("高：当日中の寄付が必要")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                }
+                
+                CustomSectionView("受け渡し場所", required: true, showWrongInputError: vm.formValidateResult == .invalidField(.adress)) {
+                    TextFieldWithMapPickerView(
+                        adress: $vm.adress,
+                        lat: $vm.lat,
+                        lng: $vm.lng,
+                        placeholder: "例：東京都千代田区"
+                    )
+                }
+                
+                CustomSectionView("連絡先", required: true, showWrongInputError: vm.formValidateResult == .invalidField(.phoneNumber)) {
+                    CustomTextFieldView("例：01234213411", text: $vm.phoneNumber, keyboardType: .phonePad)
+                } comment: {
+                    Text("※ハイパンなしで記入してください")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.gray)
+                }
+                .padding(.bottom)
+
+                CustomSectionView("コメント") {
+                    CustomCommentFieldView(comment: $vm.comment)
+                }
+                
+                ConfirmAndCancelButtonView(showErrorMessage: vm.formValidateResult != .valid, confirmButtonAction: {
+                    Task {
+                        if await vm.formIsValid() {
+                            showConfirmationView = true
+                        }
+                    }
+                })
+                .navigationDestination(isPresented: $showConfirmationView) {
+                    ConfirmationView(
+                        foodName: $vm.foodName,
+                        amount: $vm.amount,
+                        unit: $vm.unit,
+                        selectedFoodCategory: $vm.selectedFoodCategory,
+                        selectedPriority: $vm.selectedPriority,
+                        adress: $vm.adress,
+                        phoneNumber: $vm.phoneNumber,
+                        comment: $vm.comment,
+                        submit: {
+                        Task {
+                            if await vm.submit() { // 送信成功したら、Sheetを閉じる
+                                presentationMode.wrappedValue.dismiss()
+                            }
+                        }
+                    })
+                }
+
+            }
+            .padding()
+        }
+        .navigationTitle("リクエスト")
+        .background(.white)
+        .foregroundStyle(.black)
     }
 }
 
 #Preview {
-    RequestSheetView()
+    NavigationStack {
+        RequestSheetView()
+    }
 }
