53a291ae58fb45892584922bc5338296d2e03d22
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-07-05 18:01:47 +0900

    Request Modelのダミー作成

--- Fooder/Model/Request.swift
+++ Fooder/Model/Request.swift
@@ -45,3 +45,23 @@ struct Request: Codable {
         case updatedAt = "updated_at"
     }
 }
+
+
+// Dummy Data
+extension Request {
+    /// - Parameters:
+    ///   - requesterID: 寄付者のユーザーID（省略時は新しいUUID）
+    ///   - foodItemID: 使用する食品アイテムのID（省略時は新しいUUID）
+    /// - Returns: 初期化された `DonationModel` のダミーインスタンス
+    static func dummy(requesterID: String = UUID().uuidString, foodItemID: String = UUID().uuidString) -> Request {
+        return Request(
+            requesterID: requesterID,
+            foodItemID: foodItemID,
+            status: .open,
+            needBy: .tomorrow,
+            priority: .normal,
+            createdAt: .now,
+            updatedAt: .now
+        )
+    }
+}
