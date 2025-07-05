4c6bb7b2d45bbcad37cbd70160c008245e4748b3
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-07-05 16:41:55 +0900

    User モデル作成する

---
+++ Fooder/Model/User.swift
@@ -0,0 +1,54 @@
+//
+//  User.swift
+//  Fooder
+//
+//  Created by cmStudent on 2025/07/05.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: String = UUID().uuidString
+    let email: String
+    let displayName: String?
+    let userName: String
+    let avatarImageURL: String?
+    let adress: String
+    let phoneNumber: String
+    let isVerified: Bool
+    let createdAt: Date
+    let updatedAt: Date
+    
+    init(
+        email: String,
+        displayName: String?,
+        userName: String,
+        avatarImageURL: String?,
+        adress: String,
+        phoneNumber: String,
+        isVerified: Bool = false,
+        createdAt: Date = Date(),
+        updatedAt: Date = Date()
+    ) {
+        self.email = email
+        self.displayName = displayName
+        self.userName = userName
+        self.avatarImageURL = avatarImageURL
+        self.adress = adress
+        self.phoneNumber = phoneNumber
+        self.isVerified = isVerified
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, email, adress
+        case displayName = "display_name"
+        case userName = "user_name"
+        case avatarImageURL = "avatar_image_url"
+        case phoneNumber = "phone_number"
+        case isVerified = "is_verified"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
